71e548b75c936f75c29986d06e1d7e76686426fa
Ben51Degrees <ben@51degrees.com>
2020-01-08 11:57:37 +0000

    DOC: Added examples to the documentation.

---
+++ 4.1/examples.js
@@ -0,0 +1,18 @@
+var examples =
+[
+    [ "cloud/configureFromFile.js", "cloud_2configure_from_file_8js-example.html", null ],
+    [ "cloud/failureToMatch.js", "cloud_2failure_to_match_8js-example.html", null ],
+    [ "cloud/gettingStarted.js", "cloud_2getting_started_8js-example.html", null ],
+    [ "cloud/metadata.js", "cloud_2metadata_8js-example.html", null ],
+    [ "cloud/webIntegration.js", "cloud_2web_integration_8js-example.html", null ],
+    [ "hash/configureFromFile.js", "hash_2configure_from_file_8js-example.html", null ],
+    [ "hash/failureToMatch.js", "hash_2failure_to_match_8js-example.html", null ],
+    [ "hash/gettingStarted.js", "hash_2getting_started_8js-example.html", null ],
+    [ "hash/metadata.js", "hash_2metadata_8js-example.html", null ],
+    [ "hash/webIntegration.js", "hash_2web_integration_8js-example.html", null ],
+    [ "pattern/configureFromFile.js", "pattern_2configure_from_file_8js-example.html", null ],
+    [ "pattern/failureToMatch.js", "pattern_2failure_to_match_8js-example.html", null ],
+    [ "pattern/gettingStarted.js", "pattern_2getting_started_8js-example.html", null ],
+    [ "pattern/metadata.js", "pattern_2metadata_8js-example.html", null ],
+    [ "pattern/webIntegration.js", "pattern_2web_integration_8js-example.html", null ]
+];
